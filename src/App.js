08695c40d7de6e292186422fe7845097cafffcee
mahouhou <frejasmith@Frejas-MacBook-Pro.local>
2022-07-14 15:47:07 +0100

    Added annotations

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ function App() {
     </nav>
   );
   
+  // Pass props from menu data
   const NavItem = ({className, href, children, logo}) => (
     <li className={`mh2-ns f6 f4-l tc ${className}`}>
       <a className="white no-underline" href={href}>
+        {/* If logo is true, display the image, otherwise display children */}
         {logo ? <img src={require("./images/logo.svg").default} className="db center logo" /> : children}
       </a>
     </li>	
@@ -37,14 +39,19 @@ function App() {
   );
   
   const Highlight = ({colour, children}) => (
+    // Take colour attribute from Highlight component and append to classes
+    // I assume the children prop is everything contained inside the Highlight component ?
     <span className={`relative highlight highlight-${colour}`}>
       <span className="relative z-2">{children}</span>
     </span>
   );
   
+  // Pass props from attractions data
   const Overlay = ({showInfo, title, description, link}) => (
         <div  className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay"
               style={{transform: showInfo ? 'none' : 'translateY(-100%)'}}>
+              {/* Set transform to 'none' if showInfo state is true
+                  otherwise, translate (div is translated out of view by default) */}
           <div>
             <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">
               <a href={link}>{title}</a>
@@ -58,18 +65,21 @@ function App() {
     constructor(props) {
       super(props);
       this.state = {
+        // Default state is false
         showInfo: false
       };
       this.toggleInfo = this.toggleInfo.bind(this);
       this.closeInfo = this.closeInfo.bind(this);
     }
-  
+    
+    // toggleInfo function flips the showInfo state
     toggleInfo() {
       this.setState((prevState, props) => ({
         showInfo: !prevState.showInfo
       }));
     }
-  
+    
+    // closeInfo function resets the showInfo state back to false
     closeInfo() {
       this.setState({
         showInfo: false
@@ -78,6 +88,9 @@ function App() {
   
     render() {
       return (
+        // Get extra classes from attraction data
+        // Call toggleInfo function onClick event
+        // Call closeInfo function onMouseLeave event
         <div 
           className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${this.props.className}`}
           onClick={this.toggleInfo}
@@ -85,7 +98,10 @@ function App() {
         >
             <ErrorBoundary>
             <div className="relative">
+              {/* Pass props and state from Attractions to Overlay component */}
               <Overlay {...this.props} {...this.state} />
+                {/* Construct image file path from public directory
+                    add image name sourced from attractions data */}
                 <img src={"/images/" + this.props.image} className="db" />
             </div>
             </ErrorBoundary>
@@ -101,6 +117,8 @@ function App() {
         <ErrorBoundary><Intro /></ErrorBoundary>
       </div>
       <div className="flex flex-wrap container">
+        {/* Map attractions data to create Attraction components,
+            spread data onto component as attributes and values */}
         <ErrorBoundary>{attractions.map((attraction) => <Attraction {...attraction} />)}</ErrorBoundary>
       </div>
     </div>
